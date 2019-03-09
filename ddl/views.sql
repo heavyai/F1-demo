1080024d361729b92f515e8c6ff96ee2c60760e6
Randy Zwitch <rzwitch@gmail.com>
2019-03-09 16:24:00 -0500

    Add views for leaderboard

---
+++ ddl/views.sql
@@ -0,0 +1,79 @@
+--view to order laps by laptime, enriched by session info
+--tracklength clause to account for floating-point error or dropped packets
+--considered "close enough" that user probably did a valid lap
+create view v_leaderboard as
+with
+laps as (select
+sessionuid,
+currentlapnumber as lapnumber,
+playercarindex,
+min(packettime) as lapstarttime,
+max(packettime) as lapendtime,
+max(currentlaptime) as laptime,
+max(lapdistance) as lapdistance
+from gtc_lap_v2
+where playerscar = true
+group by 1,2,3),
+sessions as (select
+sessionuid,
+trackid,
+era,
+weather,
+max(airtemperature) as airtemp,
+max(tracklength) as tracklength,
+max(tracktemperature) as tracktemp
+from gtc_session_v2
+group by 1,2,3,4
+)
+select
+laps.*,
+sessions.trackid,
+sessions.era,
+sessions.weather,
+sessions.airtemp,
+sessions.tracklength,
+sessions.tracktemp
+from laps
+left join sessions on (laps.sessionuid = sessions.sessionuid)
+where lapdistance >= (0.99 * tracklength)
+order by laptime;
+
+--view to order laps by laptime, enriched by session info
+--where clause trackid = 0 represents Melbourne, Australia
+--tracklength clause to account for floating-point error or dropped packets
+--considered "close enough" that user probably did a valid lap
+create view v_leaderboard_melbourne as
+with
+laps as (select
+sessionuid,
+currentlapnumber as lapnumber,
+playercarindex,
+min(packettime) as lapstarttime,
+max(packettime) as lapendtime,
+max(currentlaptime) as laptime,
+max(lapdistance) as lapdistance
+from gtc_lap_v2
+where playerscar = true
+group by 1,2,3),
+sessions as (select
+sessionuid,
+trackid,
+era,
+weather,
+max(airtemperature) as airtemp,
+max(tracklength) as tracklength,
+max(tracktemperature) as tracktemp
+from gtc_session_v2
+group by 1,2,3,4
+)
+select
+laps.*,
+sessions.era,
+sessions.weather,
+sessions.airtemp,
+sessions.tracklength,
+sessions.tracktemp
+from laps
+left join sessions on (laps.sessionuid = sessions.sessionuid)
+where lapdistance >= (0.99 * tracklength) and trackid = 0
+order by laptime
