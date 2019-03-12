9654dca07cdb859a0274de1f77eea04daa706cd1
randyzwitch <rzwitch@gmail.com>
2019-03-12 15:59:34 -0400

    Remove original controller code
    This is now controlled by the callback

--- f1dash/controls.py
+++ f1dash/controls.py
@@ -6,39 +6,9 @@ import pymapd
 import pandas as pd
 from credentials import host, user, password, dbname, port
 
-#### helper functions
-def get_sessions():
-    #placing connection inside to avoid having stale connection
-    conn = pymapd.connect(host = host, user= user, password= password, dbname= dbname, port=port)
-
-    #query columns intentionally left vague, can optimize later
-    df = pd.read_sql("""select
-                        sessionuid,
-                        lapnumber,
-                        laptime,
-                        playercarindex
-                        from v_leaderboard_melbourne
-                        where laptime >= 60
-                        order by laptime
-                        limit 50
-                    """, conn)
-
-    return df
-
-### create leaderboard
-### leaderboard_df also used to populate reference lap dropdown
-### TODO: What happens to dropdown if this table does update in the middle, does reference lap change?
-leaderboard_df = get_sessions()
-
-#formatting for session column to make table width smaller
-leaderboard_df["session"] = [f"""S{x[-4:]}""" for x in leaderboard_df["sessionuid"]]
-
-#### reference lap: build list dynamically from leaderboard_df defined in leaderboard.py
+#### reference lap: build list dynamically with callback
 reflapmenu = dcc.Dropdown(
     id='reflapmenu',
-    options = [{'label': f"""{uid} - Lap {lnum} - {lapt}""", 'value': uid} for uid, lnum, lapt in
-                zip(leaderboard_df["session"], leaderboard_df["lapnumber"], leaderboard_df["laptime"])],
-    value=leaderboard_df["session"][0],
     searchable=False,
     clearable=False,
     style=dict(width = '275px')
