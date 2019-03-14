3f50bf073f34ddd5d1d08a82b9d960ce991ab562
randyzwitch <rzwitch@gmail.com>
2019-03-14 11:18:52 -0400

    Reference lap callback for track
    Passes in reference lap string, auto-updates every 10 seconds with 
    leaderboard for now

--- f1dash/app.py
+++ f1dash/app.py
@@ -2,15 +2,15 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
-import plotly.figure_factory as ff
 
 import pymapd
 import pandas as pd
 from credentials import host, user, password, dbname, port
 
 # import individual components from files
-from track import track
+from track import track, get_lapdata
 from leaderboard import leaderboard
 from navbar import navbar
 from telemetry import telemetry
@@ -97,7 +97,7 @@ def make_reflap_options(notused, value, values):
     df["session"] = [f"""S{x[-4:]}""" for x in df["sessionuid"]]
 
     options = [{'label': f"""{uid} - Lap {lnum} - {lapt}""",
-                'value': f"""{sessionuid}, {lstart}, {lend}, {pci}"""}
+                'value': f"""{sessionuid},{lstart},{lend},{pci}"""}
                 for uid, sessionuid, lnum, lapt, lstart, lend, pci
                 in zip(df["session"], df["sessionuid"], df["lapnumber"], df["laptime"], df["lapstarttime"], df["lapendtime"], df["playercarindex"])
               ]
@@ -112,6 +112,50 @@ def make_reflap_options(notused, value, values):
     print("Dropdown updated from database")
 
     return options, value
+
+#### Update track
+#### TODO: currently tied to leaderboard-interval update, consider second timer for more frequent updates
+@app.callback(Output('track-graph', 'figure'),
+              [Input('leaderboard-interval', 'n_intervals'), Input('reflapmenu', 'value')])
+def build_track_chart(notused, reflapvalue):
+
+    #unpack reflapvalue into parameters to use for reference lap
+    sessionuid, lapstarttime, lapendtime, _ = reflapvalue.split(',')
+    ref_lap_data = get_lapdata(sessionuid, lapstarttime, lapendtime)
+
+    #TODO: get most recent lap programmatically
+    ref_current_data = get_lapdata(1270608935058109592, "2019-03-08 23:09:37", "2019-03-08 23:11:06")
+
+    #graph properties changed here
+    trace_reference = go.Scattergl(x=ref_lap_data["worldpositionx"],
+                                   y=ref_lap_data["worldpositionz"],
+                                   mode="markers",
+                                   marker = dict(size = 2, color = "#404040"),
+                                   name="Reference Lap"
+                                  )
+
+    #TODO: set color based on another pandas series
+    trace_current = go.Scattergl(x=ref_current_data["worldpositionx"],
+                               y=ref_current_data["worldpositionz"],
+                               mode="markers",
+                               marker = dict(size = 4, color = "#1A84C7"),
+                               name="Current Lap"
+                               )
+
+    figure={
+        "data": [trace_reference, trace_current],
+        "layout": go.Layout(legend=dict(orientation="h", y=1),
+                            title='Racing Line: Current vs. Reference Lap',
+                            height=410,
+                            xaxis=dict(title='worldpositionx'),
+                            yaxis=dict(title='worldpositionz'),
+                            uirevision='never'
+                            )
+    }
+
+    print("build_track_chart fired: " + reflapvalue)
+
+    return figure
 
 #### run app
 #### TODO: disable debug when finished
--- f1dash/track.py
+++ f1dash/track.py
@@ -1,5 +1,3 @@
-import plotly.figure_factory as ff
-import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 
@@ -8,7 +6,9 @@ import pandas as pd
 from credentials import host, user, password, dbname, port
 
 #### helper functions
+#### TODO: after choosing value to color track, remove unneeded columns
 def get_lapdata(sessionuid, lapstarttime, lapendtime):
+
     #placing connection inside to avoid having stale connection
     conn = pymapd.connect(host = host, user= user, password= password, dbname= dbname, port=port)
 
@@ -16,10 +16,6 @@ def get_lapdata(sessionuid, lapstarttime, lapendtime):
     gforcelateral,
     gforcelongitudinal,
     gforcevertical,
-    packettime,
-    pitch,
-    roll,
-    sessiontime,
     worldpositionx,
     worldpositiony,
     worldpositionz,
@@ -37,44 +33,12 @@ def get_lapdata(sessionuid, lapstarttime, lapendtime):
 
     return carmotion
 
-
-#TODO: make reference lap chooseable from interface
-ref_lap_data = get_lapdata(1270608935058109592, "2019-03-08 23:11:06", "2019-03-08 23:12:34")
-
-#TODO: get most recent lap programmatically
-ref_current_data = get_lapdata(1270608935058109592, "2019-03-08 23:09:37", "2019-03-08 23:11:06")
-
-#graph properties changed here
-trace_reference = go.Scattergl(x=ref_lap_data["worldpositionx"],
-                             y=ref_lap_data["worldpositionz"],
-                             mode="markers",
-                             marker = dict(size = 2, color = "#404040"),
-                             name="Reference Lap"
-                            )
-
-#TODO: set color based on another pandas series
-trace_current = go.Scattergl(x=ref_current_data["worldpositionx"],
-                           y=ref_current_data["worldpositionz"],
-                           mode="markers",
-                           marker = dict(size = 4, color = "#1A84C7"),
-                           name="Current Lap"
-                           )
-
 #display in dash
-trackgraph = dcc.Graph(
-                    figure={
-                        "data": [trace_reference, trace_current],
-                        "layout": go.Layout(legend=dict(orientation="h", y=1),
-                                            title='Racing Line: Current vs. Reference Lap',
-                                            height=410,
-                                            xaxis=dict(title='worldpositionx'),
-                                            yaxis=dict(title='worldpositionz')
-                                            )
-                    },
-                    config={
-                        'displayModeBar': True
-                    }
-)
+trackgraph = dcc.Graph(id='track-graph',
+                       config={
+                            'displayModeBar': True
+                       }
+                      )
 
 #controls bootstrap positioning
 track = dbc.Col([trackgraph], width=6)
